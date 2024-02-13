bcbb01dee9b0f5ee3ed62fce8ba1ef4a21b961f5
amaya saudade <iwaizumi.yoshi@gmail.com>
2024-02-14 01:44:06 +0300

    create two screens and elements for them

---
+++ AboutMeApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  AboutMeApp
+//
+//  Created by Darina Kirilenko on 14.02.2024.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
