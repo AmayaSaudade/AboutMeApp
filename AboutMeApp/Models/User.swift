3413a6058b791e1aacb02d70e3d9b22221acc3f9
amaya saudade <iwaizumi.yoshi@gmail.com>
2024-02-14 18:05:19 +0300

    fix user model by adding person model

--- AboutMeApp/Models/User.swift
+++ AboutMeApp/Models/User.swift
@@ -9,11 +9,26 @@ struct User {
     let userName: String
     let password: String
     
+    let nickName: String
+    let name: String
+    let currentLocation: String
+    let department: String
+    let position: String
+    
+    let bioInfo: String
+    
     static func getUserData() -> User {
         User(
             userName: "1",
-            password: "1"
+            password: "1",
+            nickName: "Amaya",
+            name: "Darina Kirilenko",
+            currentLocation: "Arkhangelsk",
+            department: "Information security",
+            position: "ML operator",
+            bioInfo: "About me"
         )
     }
 }
+
 
